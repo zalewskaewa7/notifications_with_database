6eb60ddf29f3c91e3fbb5d189c4f8e4dee4a0e77
Ewa <zalewskaewa7@wp.pl>
2023-12-17 23:30:51 +0100

    create notifications project

---
+++ public/js/app.js
@@ -0,0 +1,169 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./resources/js/app.jsx":
+/*!******************************!*\
+  !*** ./resources/js/app.jsx ***!
+  \******************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: C:\\xampp\\htdocs\\Laravel-React\\notifications\\resources\\js\\app.jsx: Support for the experimental syntax 'jsx' isn't currently enabled (16:21):\n\n\u001b[0m \u001b[90m 14 |\u001b[39m         \u001b[36mconst\u001b[39m root \u001b[33m=\u001b[39m createRoot(el)\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 15 |\u001b[39m\u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 16 |\u001b[39m         root\u001b[33m.\u001b[39mrender(\u001b[33m<\u001b[39m\u001b[33mApp\u001b[39m {\u001b[33m...\u001b[39mprops} \u001b[33m/\u001b[39m\u001b[33m>\u001b[39m)\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m    |\u001b[39m                     \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 17 |\u001b[39m     }\u001b[33m,\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 18 |\u001b[39m     progress\u001b[33m:\u001b[39m {\u001b[0m\n\u001b[0m \u001b[90m 19 |\u001b[39m         color\u001b[33m:\u001b[39m \u001b[32m'#4B5563'\u001b[39m\u001b[33m,\u001b[39m\u001b[0m\n\nAdd @babel/preset-react (https://github.com/babel/babel/tree/main/packages/babel-preset-react) to the 'presets' section of your Babel config to enable transformation.\nIf you want to leave it as-is, add @babel/plugin-syntax-jsx (https://github.com/babel/babel/tree/main/packages/babel-plugin-syntax-jsx) to the 'plugins' section to enable parsing.\n    at constructor (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:356:19)\n    at Parser.raise (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:3223:19)\n    at Parser.expectOnePlugin (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:3269:18)\n    at Parser.parseExprAtom (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:11218:18)\n    at Parser.parseExprSubscripts (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10857:23)\n    at Parser.parseUpdate (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10840:21)\n    at Parser.parseMaybeUnary (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10816:23)\n    at Parser.parseMaybeUnaryOrPrivate (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10654:61)\n    at Parser.parseExprOps (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10659:23)\n    at Parser.parseMaybeConditional (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10636:23)\n    at Parser.parseMaybeAssign (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10597:21)\n    at C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10567:39\n    at Parser.allowInAnd (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:12284:12)\n    at Parser.parseMaybeAssignAllowIn (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10567:17)\n    at Parser.parseExprListItem (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:12020:18)\n    at Parser.parseCallExpressionArguments (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:11054:22)\n    at Parser.parseCoverCallAndAsyncArrowHead (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10964:29)\n    at Parser.parseSubscript (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10899:19)\n    at Parser.parseSubscripts (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10870:19)\n    at Parser.parseExprSubscripts (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10861:17)\n    at Parser.parseUpdate (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10840:21)\n    at Parser.parseMaybeUnary (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10816:23)\n    at Parser.parseMaybeUnaryOrPrivate (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10654:61)\n    at Parser.parseExprOps (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10659:23)\n    at Parser.parseMaybeConditional (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10636:23)\n    at Parser.parseMaybeAssign (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10597:21)\n    at Parser.parseExpressionBase (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10551:23)\n    at C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10547:39\n    at Parser.allowInAnd (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:12279:16)\n    at Parser.parseExpression (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10547:17)\n    at Parser.parseStatementContent (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:12737:23)\n    at Parser.parseStatementLike (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:12588:17)\n    at Parser.parseStatementListItem (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:12568:17)\n    at Parser.parseBlockOrModuleBlockBody (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:13189:61)\n    at Parser.parseBlockBody (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:13182:10)\n    at Parser.parseBlock (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:13170:10)\n    at Parser.parseFunctionBody (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:11935:24)\n    at Parser.parseFunctionBodyAndFinish (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:11921:10)\n    at Parser.parseMethod (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:11879:31)\n    at Parser.parseObjectMethod (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:11778:19)\n    at Parser.parseObjPropValue (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:11811:23)\n    at Parser.parsePropertyDefinition (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:11745:17)\n    at Parser.parseObjectLike (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:11660:21)\n    at Parser.parseExprAtom (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:11155:23)\n    at Parser.parseExprSubscripts (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10857:23)\n    at Parser.parseUpdate (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10840:21)\n    at Parser.parseMaybeUnary (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10816:23)\n    at Parser.parseMaybeUnaryOrPrivate (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10654:61)\n    at Parser.parseExprOps (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10659:23)\n    at Parser.parseMaybeConditional (C:\\xampp\\htdocs\\Laravel-React\\notifications\\node_modules\\@babel\\parser\\lib\\index.js:10636:23)");
+
+/***/ }),
+
+/***/ "./resources/css/app.css":
+/*!*******************************!*\
+  !*** ./resources/css/app.css ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/chunk loaded */
+/******/ 	(() => {
+/******/ 		var deferred = [];
+/******/ 		__webpack_require__.O = (result, chunkIds, fn, priority) => {
+/******/ 			if(chunkIds) {
+/******/ 				priority = priority || 0;
+/******/ 				for(var i = deferred.length; i > 0 && deferred[i - 1][2] > priority; i--) deferred[i] = deferred[i - 1];
+/******/ 				deferred[i] = [chunkIds, fn, priority];
+/******/ 				return;
+/******/ 			}
+/******/ 			var notFulfilled = Infinity;
+/******/ 			for (var i = 0; i < deferred.length; i++) {
+/******/ 				var [chunkIds, fn, priority] = deferred[i];
+/******/ 				var fulfilled = true;
+/******/ 				for (var j = 0; j < chunkIds.length; j++) {
+/******/ 					if ((priority & 1 === 0 || notFulfilled >= priority) && Object.keys(__webpack_require__.O).every((key) => (__webpack_require__.O[key](chunkIds[j])))) {
+/******/ 						chunkIds.splice(j--, 1);
+/******/ 					} else {
+/******/ 						fulfilled = false;
+/******/ 						if(priority < notFulfilled) notFulfilled = priority;
+/******/ 					}
+/******/ 				}
+/******/ 				if(fulfilled) {
+/******/ 					deferred.splice(i--, 1)
+/******/ 					var r = fn();
+/******/ 					if (r !== undefined) result = r;
+/******/ 				}
+/******/ 			}
+/******/ 			return result;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	(() => {
+/******/ 		// no baseURI
+/******/ 		
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			"/js/app": 0,
+/******/ 			"css/app": 0
+/******/ 		};
+/******/ 		
+/******/ 		// no chunk on demand loading
+/******/ 		
+/******/ 		// no prefetching
+/******/ 		
+/******/ 		// no preloaded
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 		
+/******/ 		__webpack_require__.O.j = (chunkId) => (installedChunks[chunkId] === 0);
+/******/ 		
+/******/ 		// install a JSONP callback for chunk loading
+/******/ 		var webpackJsonpCallback = (parentChunkLoadingFunction, data) => {
+/******/ 			var [chunkIds, moreModules, runtime] = data;
+/******/ 			// add "moreModules" to the modules object,
+/******/ 			// then flag all "chunkIds" as loaded and fire callback
+/******/ 			var moduleId, chunkId, i = 0;
+/******/ 			if(chunkIds.some((id) => (installedChunks[id] !== 0))) {
+/******/ 				for(moduleId in moreModules) {
+/******/ 					if(__webpack_require__.o(moreModules, moduleId)) {
+/******/ 						__webpack_require__.m[moduleId] = moreModules[moduleId];
+/******/ 					}
+/******/ 				}
+/******/ 				if(runtime) var result = runtime(__webpack_require__);
+/******/ 			}
+/******/ 			if(parentChunkLoadingFunction) parentChunkLoadingFunction(data);
+/******/ 			for(;i < chunkIds.length; i++) {
+/******/ 				chunkId = chunkIds[i];
+/******/ 				if(__webpack_require__.o(installedChunks, chunkId) && installedChunks[chunkId]) {
+/******/ 					installedChunks[chunkId][0]();
+/******/ 				}
+/******/ 				installedChunks[chunkId] = 0;
+/******/ 			}
+/******/ 			return __webpack_require__.O(result);
+/******/ 		}
+/******/ 		
+/******/ 		var chunkLoadingGlobal = self["webpackChunk"] = self["webpackChunk"] || [];
+/******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
+/******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
+/******/ 	__webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/js/app.jsx")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/css/app.css")))
+/******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
+/******/ 	
+/******/ })()
+;
