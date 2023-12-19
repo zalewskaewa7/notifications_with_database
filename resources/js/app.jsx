bd098719c264c9b1b430f640f692abb3e4a2f371
Ewa <zalewskaewa7@wp.pl>
2023-12-19 22:35:16 +0100

    update project remove install error

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,21 +1,18 @@
+import React from "react"; 
+
 import './bootstrap';
 import '../css/app.css';
 
 import { createRoot } from 'react-dom/client';
-import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
-createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
-    setup({ el, App, props }) {
-        const root = createRoot(el);
+function App(){
+    return(
+        "hello"
+    )
+}
 
-        root.render(<App {...props} />);
-    },
-    progress: {
-        color: '#4B5563',
-    },
-});
+const container = document.getElementById('app');
+const root = createRoot(container); 
+root.render(<App />);
+
